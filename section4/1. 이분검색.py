2ea5edd202b1a8899620b5719dd5e6a112a8543c
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-10-31 15:54:14 +0900

    Update 1. 이분검색.py

--- section4/1. 이분검색.py
+++ section4/1. 이분검색.py
@@ -1,10 +1,13 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 n, m=map(int, input().split())
 a=list(map(int, input().split()))
+
 a.sort()
 lt=0
 rt=n-1
+
 while lt<=rt:
     mid=(lt+rt)//2
     if a[mid]==m:
