4126832910ec75f74b71f2b7f0a6f38c56b89786
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-10-31 22:45:38 +0900

    Update 5. 회의실 배정.py

--- section4/5. 회의실 배정.py
+++ section4/5. 회의실 배정.py
@@ -1,15 +1,20 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 n=int(input())
 meeting=[]
+
 for i in range(n):
     a, b=map(int, input().split())
     meeting.append((a, b))
-meeting.sort(key=lambda x : (x[1], x[0]))
+    
+meeting.sort(key=lambda x : (x[1], x[0])) #끝시간부터 정렬 후 시작시간으로 
+
 et=0
 cnt=0
 for x, y in meeting:
     if x>=et:
         et=y
         cnt+=1
+        
 print(cnt)
