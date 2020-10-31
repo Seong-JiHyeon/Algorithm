c89c1a098752ab5e65c7417e52e7c757fa0fba36
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 00:42:23 +0900

    Update 7. 창고 정리.py

--- section4/7. 창고 정리.py
+++ section4/7. 창고 정리.py
@@ -1,12 +1,14 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 L=int(input())
 a=list(map(int, input().split()))
 m=int(input())
+
 a.sort()
-for _ in range(m):
+for _ in range(m): #한 번에 1씩 옮김 -> 총 m번 옮김
     a[0]+=1
-    a[L-1]-=1
-    a.sort()
+    a[L-1]-=1 #매번 최소값, 최대값에서 1씩 옮긴 후
+    a.sort() #다시 정렬하여 최소값, 최대값을 
 
 print(a[L-1]-a[0])
