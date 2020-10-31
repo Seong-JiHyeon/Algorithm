40d0a50dac51f7ecd452917a804cbfcbc8e00a68
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-10-31 22:01:14 +0900

    Update 3. 뮤직비디오(결정알고리즘).py

--- section4/3. 뮤직비디오(결정알고리즘).py
+++ section4/3. 뮤직비디오(결정알고리즘).py
@@ -1,5 +1,6 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 def Count(capacity):
     cnt=1
     sum=0
@@ -13,13 +14,17 @@ def Count(capacity):
 
 n, m=map(int, input().split())
 Music=list(map(int, input().split()))
+
 maxx=max(Music)
+
 lt=1
 rt=sum(Music)
+
 res=0
 while lt<=rt:
     mid=(lt+rt)//2
-    if mid>=maxx and Count(mid)<=m:
+    if mid>=maxx and Count(mid)<=m: #최대 곡 길이보다는 dvd용량이 커야 한다(maxx)
+        #앞의 문제와 달리 m개 이상을 만드는 것도 m개 포함되지 않기 때문에 res값은 <=m안에서 정의되어야 한다
         res=mid
         rt=mid-1
     else:
