6f2cb730bfa5865132dc8554efe62af23a2b9ddd
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 04:27:41 +0900

    Update 10. 역수열.py

--- section4/10. 역수열.py
+++ section4/10. 역수열.py
@@ -11,7 +11,7 @@ for i in range(n):
         if(a[i]==0 and seq[j]==0): #a[i]=0 앞 자리에 있는 숫자들 가져오기
             seq[j]=i+1 
             break
-        elif seq[j]==0: #seq만 0인 경우, 아직 할당받지 못함 -> i값이 증가하는 대신 a에서 1을 
+        elif seq[j]==0: #seq만 0인 경우, 아직 할당받지 못함 -> i값이 증가하는 대신 a에서 1을 빼줌
             a[i]-=1
 
 for x in seq:
