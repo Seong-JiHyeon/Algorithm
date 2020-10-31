591dcc2a44bcd0201b770433feab1d73102b422e
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 01:57:34 +0900

    Update 8. 침몰하는 타이타닉.py

--- section4/8. 침몰하는 타이타닉.py
+++ section4/8. 침몰하는 타이타닉.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque 
 #list는 원소를 제거하면 다른 원소들이 빈 자리에 한 칸씩 이동
-#하지만 deque는 원소를 제거하더라도 움직이지 않음 (포인터로 표시) -> 훨씬 
+#하지만 deque는 원소를 제거하더라도 움직이지 않음 (포인터로 표시) -> 훨씬 효율적
 sys.stdin=open("input.txt", "r")
 
 n, limit=map(int, input().split())
@@ -15,11 +15,11 @@ while p: #p가 비어있을 때 멈춤
     if len(p)==1: #마지막 한 명인 경우
         cnt+=1
         break
-    if p[0]+p[-1]>limit: #최소값과 최대값이 함께 탈 수 있는지 확인
-        p.pop()
+    if p[0]+p[-1]>limit: #최소값과 최대값이 함께 탈 수 없을 때
+        p.pop() #최대값만 제거
         cnt+=1
-    else:
-        p.popleft()
+    else: #함께 탈 수 있을 때
+        p.popleft() #deque의 앞 뒤로 모두 제거
         p.pop()
         cnt+=1
         
