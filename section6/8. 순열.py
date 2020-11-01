e67afee5e095217c4a45be3f2145794f7760cc29
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-02 01:13:10 +0900

    Update 8. 순열.py

--- section6/8. 순열.py
+++ section6/8. 순열.py
@@ -33,5 +33,6 @@ from itertools import permutations
 
 for i in permutations([1,2,3,4], 2):
     print(i, end=" ")
-
+#(1, 2) (1, 3) (1, 4) (2, 1) (2, 3) (2, 4) (3, 1) (3, 2) (3, 4) (4, 1) (4, 2) (4, 3)
  
+  
