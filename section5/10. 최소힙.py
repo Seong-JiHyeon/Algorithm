ee52f2c4b03a62e5080b99314a3eced5fbe6d05b
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 19:52:34 +0900

    Update 10. 최소힙.py

--- section5/10. 최소힙.py
+++ section5/10. 최소힙.py
@@ -1,17 +1,19 @@
 import sys
-import heapq as hq
+import heapq as hq #list를 heap으로 작동하도록 도와줌
 sys.stdin=open("input.txt", "r")
-a=[]
+
+a=[] #heap생성을 위해서는 list가 필요함 
 while True:
     n=int(input())
+    
     if n==-1:
         break
     if n==0:
-        if len(a)==0:
+        if len(a)==0: #heap에 자료가 없는 경우 
             print(-1)
-        else:
+        else: #heap에 root노드를 꺼내어 프린트 
             print(hq.heappop(a))
     else:
-        hq.heappush(a, n)
+        hq.heappush(a, n) #list a에 자동으로 heap 정렬 
 
 
