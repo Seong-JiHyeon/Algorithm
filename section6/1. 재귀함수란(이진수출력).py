099373100f316b748643af228dad429de52352b8
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 20:33:06 +0900

    Update 1. 재귀함수란(이진수출력).py

--- section6/1. 재귀함수란(이진수출력).py
+++ section6/1. 재귀함수란(이진수출력).py
@@ -1,11 +1,12 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 def DFS(x):
     if x==0:
-        return
+        return #종료 
     else:
         DFS(x//2)
-        print(x%2, end='')
+        print(x%2, end='') #거꾸로 출력 
 
 n=int(input())
 DFS(n)
