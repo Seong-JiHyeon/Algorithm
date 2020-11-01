37c3fc65285e62448ec210c62371aded2ed12547
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-02 00:47:57 +0900

    Update 8. 순열.py

--- section6/8. 순열.py
+++ section6/8. 순열.py
@@ -1,24 +1,28 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
+#중복순열 문제에서 중복을 제외하는 checklist를 만듦  
 def DFS(L):
     global cnt
-    if L==m:
+    if L==m: 
         for i in range(m):
             print(res[i], end=' ')
         print()
         cnt+=1
     else:
         for i in range(1, n+1):
-            if ch[i]==0:
-                ch[i]=1
-                res[L]=i
+            if ch[i]==0: #checklist에서 중복 아닌 경우에만 (!= 1)  
+                ch[i]=1 #1로 값을 바꿈으로서 순열에 사용되었다고 체크  
+                res[L]=i #순열에 포함 
                 DFS(L+1)
-                ch[i]=0
+                ch[i]=0 #checklist의 값을 다시 풀어주어야 부모노드로 돌아가서 다시 check 가능 
 
+                
 if __name__=="__main__":
     n, m=map(int, input().split())
     res=[0]*n
     ch=[0]*(n+1)
     cnt=0
+    
     DFS(0)
     print(cnt)
