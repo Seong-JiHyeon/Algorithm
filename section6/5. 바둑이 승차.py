a96768f400bfd616cb07fa442ed9b388bff6f83b
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 23:06:05 +0900

    Update 5. 바둑이 승차.py

--- section6/5. 바둑이 승차.py
+++ section6/5. 바둑이 승차.py
@@ -1,11 +1,14 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
 def DFS(L, sum, tsum):
     global result
-    if sum+(total-tsum)<result:
+    if sum+(total-tsum)<result: #앞으로 구할 최대 sum의 값이 현재 최대값을 넘지 않는 경우 가지치기 
         return
-    if sum>c:
+    
+    if sum>c: #sum이 무게제한을 넘으면 않됨 (가지치기) 
         return
+    
     if L==n:
         if sum>result:
             result=sum
@@ -14,12 +17,14 @@ def DFS(L, sum, tsum):
         DFS(L+1, sum, tsum+a[L])
 
 
+        
 if __name__=="__main__":
     c, n=map(int, input().split())
     a=[0]*n
     result=-2147000000
     for i in range(n):
-        a[i]=int(input())
+        a[i]=int(input()) #바둑이 무게
+        
     total=sum(a)
     DFS(0, 0, 0)
     print(result)
