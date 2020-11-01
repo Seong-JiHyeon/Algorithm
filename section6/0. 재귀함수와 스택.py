b13ca0767952c4d158c5a81d03fb4707bdfca4fc
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-01 20:33:42 +0900

    Update 0. 재귀함수와 스택.py

--- section6/0. 재귀함수와 스택.py
+++ section6/0. 재귀함수와 스택.py
@@ -3,9 +3,9 @@ sys.stdin = opne("input.txt", "r")
 
 def DFS(x):
    if x>0:
-      print(x) #(3, 2, 1) 역순으로 출력
+      print(x) #(3, 2, 1) 역순으로 출력, but 순서대로 출력한 것이다
       DFS(x-1)
-      print(x) #(1, 2, 3) 순서대로 출력 
+      print(x) #(1, 2, 3) 순서대로 출력, but 거꾸로 출력한 
      
  if __name__ == "__main__":
     n = int(input())
