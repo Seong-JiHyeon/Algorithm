b265ebc5e61d5f6055783582fca509435419c99f
Seong-JiHyeon <56622667+Seong-JiHyeon@users.noreply.github.com>
2020-11-08 23:34:45 +0900

    Update 4. 동전바꿔주기.py

--- section7/4. 동전바꿔주기.py
+++ section7/4. 동전바꿔주기.py
@@ -1,15 +1,18 @@
 import sys
 sys.stdin=open("input.txt", "r")
+
+#중복 순열 문제
 def DFS(L, sum):
     global cnt
-    if sum>m:
+    if sum>m: #지폐의 합보다 sum이 더 큰 경우
         return
-    if L==n:
-        if sum==m:
+    if L==n: #모든 동전의 경우를 다 계산한 경우
+        if sum==m: #지폐와 값이 같을 때에만 count
             cnt+=1
     else:
-        for i in range(cn[L]+1):
+        for i in range(cn[L]+1): #동전을 가지고 있는 계수만큼 for loop
             DFS(L+1, sum+(cv[L]*i))
+            
 
 m=int(input())
 n=int(input())
